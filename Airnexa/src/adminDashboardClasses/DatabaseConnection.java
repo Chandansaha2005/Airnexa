342a56af195133ec0017c141b0d2f70c27e72066
Chandan Saha <chandansaha1945@gmail.com>
2025-09-17 12:27:10 +0530

    made  database connection class to use

--- Airnexa/src/adminDashboardClasses/DatabaseConnection.java
+++ Airnexa/src/adminDashboardClasses/DatabaseConnection.java
@@ -8,6 +8,22 @@ package adminDashboardClasses;
  *
  * @author CHANDAN
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
-    
+
+    private static final String URL = "jdbc:mysql://localhost:3306/airline?useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234567k"; // Change to your password
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found", e);
+        }
+    }
 }
